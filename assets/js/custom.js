8c7259bc007245bd708ad6a1030d9656edd69b0f
Andreas Panayiotou <antreas20197@gmail.com>
2022-07-09 20:23:44 +0300

    Styling and News Page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,30 @@
-$.get('publications.html', null, function(text){
-    console.log($(text).find('#siggraph22-ccp'));
+var stringToHTML = function (str) {
+	var parser = new DOMParser();
+	var doc = parser.parseFromString(str, 'text/html');
+	return doc.body;
+};
+
+$.get('news.html', null, function(text){
+    var newsContainer = $(text).find('#newsContainer')[0].innerHTML;
+    var newsHTML = stringToHTML(newsContainer);
+    var newsRows = newsHTML.querySelectorAll('.newsItem')
+
+    //SELECT HOW MANY POSTS TO SHOW IN HOMESCREEN FROM NEWS.HTML PAGE
+    var numOfNewsInHomepage = 5;
+    var newsContainerHomepage = document.getElementById("newsContainerHomepage");
+
+    var i = 0;
+    while (i < numOfNewsInHomepage && i < newsRows.length) {
+        var textRow = newsRows[i].innerHTML;
+        var htmlRow = stringToHTML(textRow);
+        newsContainerHomepage.appendChild(htmlRow.firstChild);
+        i++;
+    }
+
+    if(numOfNewsInHomepage < newsRows.length){
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute('class', 'moreNewsBtn');
+        newDiv.innerHTML = '<a href="news.html">SEE OLDER POSTS</a>'
+        newsContainerHomepage.appendChild(newDiv);
+    }
 });
