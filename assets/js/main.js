0b7fe488f2c466f29e276af8ea6cd8ba74f6c359
Andreas Panayiotou <antreas20197@gmail.com>
2022-07-14 19:16:41 +0300

    Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,41 @@
 * License: https://bootstrapmade.com/license/
 */
 
+$(function(){
+  if($('body').is('.mainPage')){
+    var stringToHTML = function (str) {
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(str, 'text/html');
+      return doc.body;
+    };
+    
+    $.get('news.html', null, function(text){
+        var newsContainer = $(text).find('#newsContainer')[0].innerHTML;
+        var newsHTML = stringToHTML(newsContainer);
+        var newsRows = newsHTML.querySelectorAll('.newsItem')
+    
+        //SELECT HOW MANY POSTS TO SHOW IN HOMESCREEN FROM NEWS.HTML PAGE
+        var numOfNewsInHomepage = 5;
+        var newsContainerHomepage = document.getElementById("newsContainerHomepage");
+    
+        var i = 0;
+        while (i < numOfNewsInHomepage && i < newsRows.length) {
+            var textRow = newsRows[i].innerHTML;
+            var htmlRow = stringToHTML(textRow);
+            newsContainerHomepage.appendChild(htmlRow.firstChild);
+            i++;
+        }
+    
+        if(numOfNewsInHomepage < newsRows.length){
+            var newDiv = document.createElement("div");
+            newDiv.setAttribute('class', 'moreNewsBtn');
+            newDiv.innerHTML = '<a href="news.html">SEE OLDER POSTS</a>'
+            newsContainerHomepage.appendChild(newDiv);
+        }
+    });
+  }
+});
+
 (function() {
   "use strict";
 
